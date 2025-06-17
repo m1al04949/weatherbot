19b27ee83584bbe5bf85fc929a9e78435ba2c8dc
Ivan Gorbunov <m1al04949@gmail.com>
2025-06-17 17:51:05 +0300

    Add buttons

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -23,6 +23,17 @@ func New(log *slog.Logger, bot *tgbotapi.BotAPI, owClient *openweather.OpenWeath
 	}
 }
 
+func (h *Handler) Start() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := h.bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		h.handlerUpdate(update)
+	}
+}
+
 func (h *Handler) handlerUpdate(update tgbotapi.Update) {
 	if update.Message == nil {
 		return
@@ -33,13 +44,31 @@ func (h *Handler) handlerUpdate(update tgbotapi.Update) {
 		slog.String("username", update.Message.From.UserName),
 		slog.String("message", update.Message.Text))
 
+	// /start
+	if update.Message.Text == "/start" || update.Message.Text == "Назад" {
+		h.messageStart(update.Message.Chat.ID)
+		return
+	}
+
+	// Handle Mode
+	if update.Message.Text == "Ввести вручную" {
+		h.messageOther(update.Message.Chat.ID)
+		return
+	}
+
+	replyKeyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Назад"),
+		),
+	)
+
 	// Get coordinates
 	cord, err := h.owClient.Coordinates(update.Message.Text)
 	if err != nil {
 		h.log.Error(err.Error())
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Такой населенный пункт не найден")
 		msg.ReplyToMessageID = update.Message.MessageID
-
+		msg.ReplyMarkup = replyKeyboard
 		h.bot.Send(msg)
 		return
 	}
@@ -52,6 +81,7 @@ func (h *Handler) handlerUpdate(update tgbotapi.Update) {
 			update.Message.Chat.ID,
 			fmt.Sprintf("Температура в населенном пункте %s не определена", update.Message.Text))
 		msg.ReplyToMessageID = update.Message.MessageID
+		msg.ReplyMarkup = replyKeyboard
 		h.bot.Send(msg)
 		return
 	}
@@ -60,16 +90,28 @@ func (h *Handler) handlerUpdate(update tgbotapi.Update) {
 		update.Message.Chat.ID,
 		fmt.Sprintf("Температура в населенном пункте %s: %d °C", update.Message.Text, int(math.Round(weather.Temp))))
 	msg.ReplyToMessageID = update.Message.MessageID
+	msg.ReplyMarkup = replyKeyboard
 	h.bot.Send(msg)
 }
 
-func (h *Handler) Start() {
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+func (h *Handler) messageStart(id int64) {
+	replyKeyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Санкт-Петербург"),
+			tgbotapi.NewKeyboardButton("Ввести вручную"),
+		),
+	)
 
-	updates := h.bot.GetUpdatesChan(u)
+	msg := tgbotapi.NewMessage(id, "Узнать погоду в населенном пункте")
+	msg.ReplyMarkup = replyKeyboard
 
-	for update := range updates {
-		h.handlerUpdate(update)
-	}
+	h.bot.Send(msg)
+}
+
+func (h *Handler) messageOther(id int64) {
+
+	msg := tgbotapi.NewMessage(id, "Введите имя населенного пункта")
+	msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+
+	h.bot.Send(msg)
 }
