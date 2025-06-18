931828232c98e6796b8219b1c2a8f103b5252bb7
Ivan Gorbunov <m1al04949@gmail.com>
2025-06-18 18:13:28 +0300

    Add forecast model

--- internal/models/models.go
+++ internal/models/models.go
@@ -12,11 +12,38 @@ type CordinatesResponse struct {
 }
 
 type Weather struct {
-	Temp float64
+	Date        string
+	Description string
+	Temp        float64
+	Humidity    int64
+	Speed       float64
 }
 
 type WeatherResponse struct {
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
 	Main struct {
-		Temp float64 `json:"temp"`
+		Temp     float64 `json:"temp"`
+		Humidity int64   `json:"humidity"`
 	} `json:"main"`
+	Wind struct {
+		Speed float64 `json:"speed"`
+	} `json:"wind"`
+}
+
+type ForecastWeatherResponse struct {
+	List []struct {
+		Main struct {
+			Temp     float64 `json:"temp"`
+			Humidity int64   `json:"humidity"`
+		} `json:"main"`
+		Weather []struct {
+			Description string `json:"description"`
+		} `json:"weather"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+		} `json:"wind"`
+		Date string `json:"dt_txt"`
+	} `json:"list"`
 }
