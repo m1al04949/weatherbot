e2837617d64e51514054ed8011f6c888937bfa62
Ivan Gorbunov <m1al04949@gmail.com>
2025-06-18 18:13:22 +0300

    Add forecast handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -7,6 +7,7 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/m1al04949/weatherbot/internal/clients/openweather"
+	"github.com/m1al04949/weatherbot/internal/models"
 )
 
 type Handler struct {
@@ -14,6 +15,8 @@ type Handler struct {
 	bot      *tgbotapi.BotAPI
 	owClient *openweather.OpenWeatherClient
 }
+
+var currentLocation models.CordinatesResponse
 
 func New(log *slog.Logger, bot *tgbotapi.BotAPI, owClient *openweather.OpenWeatherClient) *Handler {
 	return &Handler{
@@ -56,8 +59,15 @@ func (h *Handler) handlerUpdate(update tgbotapi.Update) {
 		return
 	}
 
+	// Forecast
+	if update.Message.Text == "Прогноз" {
+		h.messageForecast(update)
+		return
+	}
+
 	replyKeyboard := tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Прогноз"),
 			tgbotapi.NewKeyboardButton("Назад"),
 		),
 	)
@@ -73,22 +83,32 @@ func (h *Handler) handlerUpdate(update tgbotapi.Update) {
 		return
 	}
 
-	// Get weather
-	weather, err := h.owClient.Weather(cord.Lat, cord.Lon)
+	// Get Current weather
+	currentLocation.Name = update.Message.Text
+	currentLocation.Lat = cord.Lat
+	currentLocation.Lon = cord.Lon
+	weather, err := h.owClient.CurrentWeather(cord.Lat, cord.Lon)
 	if err != nil {
 		h.log.Error(err.Error())
 		msg := tgbotapi.NewMessage(
 			update.Message.Chat.ID,
-			fmt.Sprintf("Температура в населенном пункте %s не определена", update.Message.Text))
+			fmt.Sprintf("Погода в населенном пункте %s не определена", update.Message.Text))
 		msg.ReplyToMessageID = update.Message.MessageID
 		msg.ReplyMarkup = replyKeyboard
 		h.bot.Send(msg)
 		return
 	}
-
 	msg := tgbotapi.NewMessage(
 		update.Message.Chat.ID,
-		fmt.Sprintf("Температура в населенном пункте %s: %d °C", update.Message.Text, int(math.Round(weather.Temp))))
+		fmt.Sprintf(`Погода в населенном пункте %s.
+Температура %d °C, %s.
+Влажность %d%%.
+Скорость ветра %d м/с.`,
+			update.Message.Text,
+			int(math.Round(weather.Temp)), weather.Description,
+			weather.Humidity,
+			int(math.Round(weather.Speed)),
+		))
 	msg.ReplyToMessageID = update.Message.MessageID
 	msg.ReplyMarkup = replyKeyboard
 	h.bot.Send(msg)
@@ -98,6 +118,13 @@ func (h *Handler) messageStart(id int64) {
 	replyKeyboard := tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Санкт-Петербург"),
+			tgbotapi.NewKeyboardButton("Москва"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Коломна"),
+			tgbotapi.NewKeyboardButton("Орск"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Ввести вручную"),
 		),
 	)
@@ -113,5 +140,43 @@ func (h *Handler) messageOther(id int64) {
 	msg := tgbotapi.NewMessage(id, "Введите имя населенного пункта")
 	msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
 
+	h.bot.Send(msg)
+}
+
+func (h *Handler) messageForecast(update tgbotapi.Update) {
+
+	var text string
+
+	replyKeyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Назад"),
+		),
+	)
+
+	forecast, err := h.owClient.ForecastWeather(currentLocation.Lat, currentLocation.Lon)
+	if err != nil {
+		h.log.Error(err.Error())
+		msg := tgbotapi.NewMessage(
+			update.Message.Chat.ID,
+			fmt.Sprintf("Прогноз погоды в населенном пункте %s не определен", update.Message.Text))
+		msg.ReplyToMessageID = update.Message.MessageID
+		msg.ReplyMarkup = replyKeyboard
+		h.bot.Send(msg)
+		return
+	}
+
+	for _, day := range *forecast {
+		if text == "" {
+			text = fmt.Sprintf("Прогноз в населенном пункте %s. \n",
+				currentLocation.Name)
+		}
+
+		text = text + fmt.Sprintf("%s. Температура %d °C, %s. Влажность %d%%. Скорость ветра %d м/с. \n",
+			day.Date, int(math.Round(day.Temp)), day.Description, day.Humidity, int(math.Round(day.Speed)))
+	}
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+	msg.ReplyToMessageID = update.Message.MessageID
+	msg.ReplyMarkup = replyKeyboard
 	h.bot.Send(msg)
 }
